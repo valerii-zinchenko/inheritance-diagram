9ac01224a4339c6113a4f03d1c7cbaed44bd5da1
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2016-11-23 00:45:59 +0200

    init: finalize the input adapter
    Draft interface was defined and base implementation is done. All output
    data from the adapter are the instances of GraphNode.

--- src/inputAdapter.js
+++ src/inputAdapter.js
@@ -1,21 +1,94 @@
-function collectParents(nodeName, map) {
-	var stack = [nodeName];
+var Class = require('class-wrapper').Class;
+var GraphNode = require('./src/GraphNode');
 
-	var node = map[nodeName];
-	if (node && node.parent) {
-		stack = stack.concat(collectParents(node.parent, map));
+
+var InputAdapter = Class(function(properties) {
+	if (properties) {
+		Object.keys(properties).forEarch(property => {
+			if (typeof this[property] === 'function') {
+				console.warn(`InputAdapter(): property ${property} is skipped because this name is already reserved for a method`);
+
+				return;
+			}
+
+			var value = properties[property];
+			if (typeof value === 'undefined') {
+				console.warn(`InputAdapter(): property ${property} is skipped because it's value is undefined`);
+
+				return;
+			}
+
+			this[property] = properties[property];
+		});
 	}
+}, {
+	noi: null,
+	map: null,
 
-	return stack;
-}
+	setNodeMap: function(map) {
+		if (!(map instanceof Object)) {
+			throw new TypeError('"map" argument is expected to be an instance of Object class');
+		}
 
-var map2Array = function(nodeName, map) {
-	var nodes = [];
+		this.map = map;
+	},
 
-	if (!map[nodeName]) {
-		throw new Error(`Node name "${nodeName}" does not exist in the provided node map`);
+	prepareNOI: function(noiName) {
+		this.noi = null;
+
+		if (typeof noiName !== 'string') {
+			throw new TypeError('"noi" argument is expected to be a string');
+		}
+
+		var noiData = this.map[noiName];
+		if (!noiData) {
+			throw new Error(`Node data for "${noiName}" does not exist in the provided node map`);
+		}
+
+		this.noi = new GraphNode(noiData, {
+			parentStack: noiData.parent ? this.prepareParentNodes(noiData.parent) : undefined
+		});
+
+		return this.noi;
+	},
+
+	prepareSecondaryNodes: function() {
+		var others = [];
+
+		[
+			'children',
+			'mixin'
+		].forEach(nodeSet => {
+			var set = this.noi[nodeSet];
+
+			if (!set) {
+				return;
+			}
+
+			set.forEach(nodeName => {
+				var data = this.map[nodeName];
+				others.push(new GraphNode(data, {
+					type: data ? nodeSet : 'no-ref'
+				}));
+			});
+		});
+
+		return others;
+	},
+
+	prepareParentNodes: function(nodeName) {
+		var data = this.map[nodeName];
+
+		var stack = [new GraphNode(data, {
+			type: data ? 'parent' : 'no-ref'
+		})];
+
+		if (data && data.parent) {
+			stack = stack.concat(this.prepareParentNodes(data.parent));
+		}
+
+		return stack;
 	}
+});
 
-	return nodes.concat(collectParents(nodeName, map));
-}
-
+module.exports = InputAdapter;
