74c07e29df9701da6e795e22b8b5532b460eddc8
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2016-11-29 23:34:19 +0200

    init: draft output adapter

--- src/outputAdapter.js
+++ src/outputAdapter.js
@@ -1,5 +1,12 @@
-function d3ToStandaloneSVG (d3svg, styleFileName) {
-	var str = ''; //'<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+function d3ToStandaloneSVG(d3svg, styleFileName) {
+	var css = '';
+
+	var domStyle = (domSvg.select('defs') || domSvg.append('defs'))
+			.append('style')
+			.attr('type', 'text/css');
+	domStyle.text('<![CDATA[' + css + ']]');
+
+	var str = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
 
 	str += d3svg.text();
 
