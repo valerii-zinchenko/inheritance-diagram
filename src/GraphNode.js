f0b36b7d0de8447c8d0606805ae3e2994f4a26b8
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2016-12-08 00:33:06 +0200

    init: replace node data only if it exists; provide default value for the node name

--- src/GraphNode.js
+++ src/GraphNode.js
@@ -9,23 +9,25 @@ var Class = require('class-wrapper').Class;
  * @param {Object} [properties] - Properties for a graph node. Any of already defined properties can be defined and new one can be added. Only property names which are already defined for methods or if the value is undefined, then such properties will be skipped and warnong message will be displayed in the console.
  */
 var GraphNode = Class(function(data, properties) {
-	this.data = data;
+	if (data) {
+		this.data = data;
 
-	// set references to some raw data properties for easier access
-	// --------------------------------------------------
-	if (data.parent) {
-		this.parent = data.parent;
+		// set references to some raw data properties for easier access
+		// --------------------------------------------------
+		if (data.parent) {
+			this.parent = data.parent;
+		}
+		if (data.children) {
+			this.children = data.children;
+		}
+		if (data.mixins) {
+			this.mixins = data.mixins;
+		}
+		// --------------------------------------------------
 	}
-	if (data.children) {
-		this.children = data.children;
-	}
-	if (data.mixins) {
-		this.mixins = data.mixins;
-	}
-	// --------------------------------------------------
 
 	if (properties) {
-		Object.keys(properties).forEarch(property => {
+		Object.keys(properties).forEach(property => {
 			if (typeof this[property] === 'function') {
 				console.warn(`GraphNode(): property ${property} is skipped because this name is already reserved for a method`);
 
@@ -43,6 +45,20 @@ var GraphNode = Class(function(data, properties) {
 		});
 	}
 }, /** @lends GraphNode.prototype */{
+	/**
+	 * Node data
+	 *
+	 * @type {Object}
+	 */
+	data: {},
+
+	/**
+	 * Node name
+	 *
+	 * @type {String}
+	 */
+	name: '',
+
 	/**
 	 * Name of parent node
 	 *
