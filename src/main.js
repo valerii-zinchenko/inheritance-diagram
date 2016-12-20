d255915ce23f5de725f98ba0ad1ad1b0e3dda99b
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2016-12-20 23:38:02 +0200

    init: fix reference

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ var Diagram = Class(function(noiName, nodeMap, css) {
 	this.positing.position(this.noi);
 
 	this.domContainer = this.rendering.render(this.noi);
-	var out = this.outAdapter(domContainer, css);
+	var out = this.outAdapter(this.domContainer, css);
 
 	fs.writeFile('out.svg', out, err => {
 		if (err) {
