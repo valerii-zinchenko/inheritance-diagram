d26f219f4ae6d8a569950a2b9e49ecb2272ba65f
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2017-01-14 22:24:56 +0200

    chore: update the example script

--- example.js
+++ example.js
@@ -3,19 +3,21 @@
 var graph = new (require('./'))('node', {
 	node: {
 		parent: 'parentNode',
-		children: ['child', 'child 2'],
+		children: ['child', 'child2'],
 		mixins:['mixin', 'mixin2', 'mixin3']
 	},
 	parentNode: {
+		parent: 'Object',
 		children: ['node'],
 		link: '#parentNode'
+	},
+	child2: {
+		link: '#child2'
+	},
+	mixin3: {
+		link: '#mixin3'
 	}
-}, `
-	rect {stroke-width: 2; stroke: blue; fill: white;}
-	path {stroke-width: 2; stroke: black; fill: none}
-	marker path {stroke-width:0; fill:black;}
-	.mixin {stroke: green;}
-`);
+}, '.no-ref rect {fill: lightgray;}');
 
 
 const fileName = 'out.svg';
