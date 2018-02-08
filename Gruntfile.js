e54f569d69da99f3de068fd506430ca3618b2ecf
Valerii Zinchenko <zinchenko.valerii@gmail.com>
2018-02-09 00:12:20 +0200

    chore: update the code coverage tool
    Replace "istanbul" to "nyc", which is also "istanbul" but like a newer or something like that.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,30 +6,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'Spec',
-					ui: 'tdd',
-					root: './src'
-				},
-				src: ['<%= pkg.directories.test %>/*.js']
-			}
-		},
-
-		mocha_istanbul: {
-			test: {
-				options: {
-					ui: 'tdd',
-					root: './src',
-					coverage: {
-						htmlReport: '<%= pkg.directories.coverage %>'
-					}
-				},
-				src: ['<%= pkg.directories.test %>/*.js']
-			}
-		},
-
 		jsdoc: {
 			options: {
 				configure: 'jsdoc.conf.json',
@@ -58,37 +34,16 @@ module.exports = function(grunt) {
 					'commonjs',
 					'mocha'
 				],
-				fix: true
+				fix: false
 			},
 			target: [src + '*.js', '<%= pkg.directories.test %>/*.js', '!<%= pkg.directories.test %>/polifills.js']
 		}
 	});
 
 
-	grunt.registerMultiTask('prepareForCoverage', 'Generates coverage reports for JS using Istanbul', function() {
-		var istanbul = require('istanbul');
-		var ignore = this.data.ignore || [];
-		var instrumenter = new istanbul.Instrumenter();
-
-		this.files.forEach(function(file) {
-			var filename = file.src[0];
-			var instrumented = grunt.file.read(filename);
-
-			if (!grunt.file.isMatch(ignore, filename)) {
-				instrumented = instrumenter.instrumentSync(instrumented, filename);
-			}
-
-			grunt.file.write(file.dest, instrumented);
-		});
-	});
-
-
 	[
-		['test', ['mochaTest']],
 		['lint', ['eslint']],
-		['coverage', ['mocha_istanbul']],
-		['doc', ['jsdoc']],
-		['gh-pages', ['coverage', 'doc']]
+		['doc', ['jsdoc']]
 	].forEach(function(registry){
 		grunt.registerTask.apply(grunt, registry);
 	});
